5f0a8003921bcdee146d01e1ea78e9bd44e2e4a6
raqushka <toster721@gmail.com>
2015-05-21 15:06:24 +0300

    change list on jason file, add file for logins, add folder for css etc

--- src/js/main.js
+++ src/js/main.js
@@ -14,34 +14,19 @@ dracApp.config(function($routeProvider) {
             controller : 'rbListController'
         })
     });
+
 dracApp.controller('mainController', function($scope) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
 });
-dracApp.controller('rbListController', function(){
-    var rbList;
-    rbList = {
-        ember : {
-            name: 'ember',
-            lvl: '80',
-            time: '280',
-            live: 0
-        },
-        khaki : {
-            name: 'khaki',
-            lvl: '78',
-            time: '120',
-            live: 1
-        },
-        zaken : {
-            name: 'zaken',
-            lvl: '75',
-            time: '450',
-            live: 1
-        }
-    }
-});
+
 dracApp.controller('aboutController', function($scope) {
     // create a message to display in our view
     $scope.message = 'aboutController';
+});
+
+dracApp.controller('rbListController', function($scope, $http){
+    $http.get('src/json/rbs.json').success(function(data){
+        $scope.rbLists = data;
+    });
 });
